e1452dec164e6e4a8a350775a434d29e11d3445e
Saul Paredes <sparedesq@gmail.com>
2017-10-11 11:54:52 -0500

    Hanoi class implemented
    some vital functions missing

--- hanoi.cpp
+++ hanoi.cpp
@@ -2,45 +2,49 @@
 #include <vector>
 #include <stack>
 #include <unordered_map>
+#include <cstdlib> /* exit, EXIT_FAILURE */
 
-void play(const int & n);
-void printStacks(std::vector <std::stack<int>> & stacks);
-void move(std::stack<int> & from, std::stack<int> & to, int & ru);
+class Hanoi
+{
+public:
+  Hanoi(const unsigned int &);
+  void play();
+  
+private:
+  std::vector <std::stack<int>> stacks; // the 3 stacks
+  std::unordered_map <int, int> moves; // maps disks to their last position before current one
+  int lrud; // least recently used disk
+
+  void move(std::stack<int> &, std::stack<int> &);
+  void printStacks(); // called by move to output process
+};
+
 
 int main()
 {
-  const int DISKS = 5;
-  play(DISKS);
+  const unsigned int DISKS = 5;
+  Hanoi hanoi(DISKS);
 }
 
-void play(const int & n)
+Hanoi::Hanoi(const unsigned int & n):
+  stacks{3}, // 3 stacks
+  moves{n}, // moves is size n
+  lrud{-1} // no recently used disk
 {
   if(n < 1)
     {
-      std::cout << "Please choose a positive number of disks\n";
-      return;
+      std::cerr << "Please choose a positive number of disks\n";
+      exit(EXIT_FAILURE);
     }
-  
-  std::vector <std::stack<int>> stacks (3); // always 3 possible positions
-  std::unordered_map <int, int> moves; // maps disk to it's last position
-  int lrud = -1; // keeps track of least recently used disk
-  
+
   for(int i = n - 1; i >= 0; i--) // the disks are zero-based
-    {
-      stacks[0].push(i); // all discs start on the left(stacks[0])
-    }
-  
+    stacks[0].push(i); // all discs start on the left stack (stacks[0])
+
   std::cout << "initial position:\n";
-  printStacks(stacks);
-  
+  printStacks();
 }
 
-void move(std::vector<std::stack<int>> & stacks, std::stack<int> & from, std::stack<int> & to, int & ru)
-{
-  
-}
-
-void printStacks(std::vector <std::stack<int>> & stacks)
+void Hanoi::printStacks()
 {
   int i = 0;
   for(const auto & stack : stacks)
@@ -51,4 +55,19 @@ void printStacks(std::vector <std::stack<int>> & stacks)
       std::cout << '\n';
     }
   std::cout << '\n';
+}
+
+void Hanoi::move(std::stack<int> & from, std::stack<int> & to)
+{
+  int fromPosition;
+  for(int i = 0; i < 3; i++)
+    if (stacks[i] == from)
+      fromPosition = i;
+
+  moves[from.top()] = fromPosition; // remember from last position before moving it to "to" 
+  lrud = from.top(); // lrud is the disk being moved "from" to "to"
+  to.push(from.top()); // move disk
+  from.pop(); // pop it from "from"
+
+  printStacks();
 }
